250f2d7fbc09633028591278fc9aa4922e3dcf87
HYPech <TCTORBAO@GMAIL.COM>
2021-01-24 09:07:40 -0500

    Initial commit

---
+++ lib/src/main/java/com/hypech/lib/C2_HelloDate.java
@@ -0,0 +1,9 @@
+package com.hypech.lib;
+
+import java.util.*;
+public class C2_HelloDate {
+    public static void main(String[] args){
+        System.out.println("hellow it is :");
+        System.out.println(new Date());
+    }
+}
